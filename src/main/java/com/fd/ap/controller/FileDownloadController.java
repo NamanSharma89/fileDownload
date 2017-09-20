482079188e7a39974c8dbb1a62f40c40c5a5c5b0
NamanSharma89 <dev.namansharma89@gmail.com>
2017-09-20 21:42:03 +0530

    new method to handle another file for download

--- src/main/java/com/fd/ap/controller/FileDownloadController.java
+++ src/main/java/com/fd/ap/controller/FileDownloadController.java
@@ -21,8 +21,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class FileDownloadController {
 	
-//	private static final String INTERNAL_FILE="irregular-verbs-list.pdf";
 	private static final String EXTERNAL_FILE_PATH="G:/Dev/plan/";
+	private static final String EXTERNAL_FILE_PATH_FOR_DATA_FILES="G:/Dev/plan/";
 	
 
 	@RequestMapping(value={"/","/welcome"}, method = RequestMethod.GET)
@@ -41,8 +41,6 @@ public class FileDownloadController {
 		File file = null;
 		
 		if(type.equalsIgnoreCase("external") && fileNumber!=0){
-//			ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-//			file = new File(classloader.getResource(INTERNAL_FILE).getFile());
 			file = new File(EXTERNAL_FILE_PATH + fileNumber + ".html");
 			}else{
 				String errorMessage = "Sorry. The file NUMBER you are looking for does not exist";
@@ -84,5 +82,54 @@ public class FileDownloadController {
         //Copy bytes from source to destination(outputstream in this example), closes both streams.
         FileCopyUtils.copy(inputStream, response.getOutputStream());
 	}
+	
+	@RequestMapping(value="/download/{app}", method = RequestMethod.GET)
+	public void downloadDataFiles(HttpServletResponse response, @PathVariable("app") String app ) throws IOException {
+	
+		File file = null;
+		
+		if(app.equalsIgnoreCase("drs") ||  app.equalsIgnoreCase("cars")){
+			file = new File(EXTERNAL_FILE_PATH_FOR_DATA_FILES + "data.tar");
+			}else{
+				String errorMessage = "Sorry, data.tar you are looking for does not exist";
+				System.out.println(errorMessage);
+			
+		}
+		
+		if(!file.exists()){
+			String errorMessage = "Sorry. The file you are looking for does not exist";
+			System.out.println(errorMessage);
+			OutputStream outputStream = response.getOutputStream();
+			outputStream.write(errorMessage.getBytes(Charset.forName("UTF-8")));
+			outputStream.close();
+			return;
+		}
+		
+		String mimeType= URLConnection.guessContentTypeFromName(file.getName());
+		if(mimeType==null){
+			System.out.println("mimetype is not detectable, will take default");
+			mimeType = "application/octet-stream";
+		}
+		
+		System.out.println("mimetype : "+mimeType);
+		
+        response.setContentType(mimeType);
+        
+        /* "Content-Disposition : inline" will show viewable types [like images/text/pdf/anything viewable by browser] right on browser 
+            while others(zip e.g) will be directly downloaded [may provide save as popup, based on your browser setting.]*/
+        response.setHeader("Content-Disposition", String.format("attachment; filename=\"" + file.getName() +"\""));
+
+        
+        /* "Content-Disposition : attachment" will be directly download, may provide save as popup, based on your browser setting*/
+        //response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+        
+        response.setContentLength((int)file.length());
+
+		InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+
+        //Copy bytes from source to destination(outputstream in this example), closes both streams.
+        FileCopyUtils.copy(inputStream, response.getOutputStream());
+	}
+	
 
 }
